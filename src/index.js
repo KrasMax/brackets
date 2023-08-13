892706342c3af07b937dabf460013b69193d3b0f
KrasMax <maximkraskovskiy@gmail.com>
2023-08-13 15:11:07 +0300

    complited task. 3 err

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+
+    var stack = [];
+    var last;
+  
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] == '[' || str[i] == '(' || str[i] == '{') {
+            stack.push(str[i]);
+        } else if (str[i] == ']' || str[i] == ')' || str[i] == '}') { 
+
+            if (stack.length) { 
+
+                last = stack[stack.length - 1]; 
+
+                if ((last == '[' && str[i] == ']') || (last == '(' && str[i] == ')') || (last == '{' && str[i] == '}')) {
+                    
+                    stack.pop();
+                }
+            } else return false; 
+        }
+    }
+    return (!stack.length);
+  
 }
